b8355456308ce0b3ad9a2afc3fb3bae92f8d9a10
Marek Sroczkowski <marekes97@gmail.com>
2021-02-23 00:03:45 +0100

    Added properties for login and password in user entity

--- TutoringSystem/TutoringSystemLib/Entities/User.cs
+++ TutoringSystem/TutoringSystemLib/Entities/User.cs
@@ -7,6 +7,8 @@ namespace TutoringSystemLib.Entities
     public class User
     {
         public int Id { get; set; }
+        public string Login { get; set; }
+        public string PasswordHash { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public Role Role { get; set; }
