c3eacecff29af1c4d37862ff7dff88bbfb8a7d6e
Marek Sroczkowski <marekes97@gmail.com>
2021-02-22 21:53:50 +0100

    Added entities class for place

---
+++ TutoringSystem/TutoringSystemLib/Entities/Place.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TutoringSystemLib.Entities
+{
+    public class Place
+    {
+        public int Id { get; set; }
+        public bool AtTutor { get; set; }
+        public string Address { get; set; }
+    }
+}
