e1df085f4acf215dd292a81e945fc7c076357ce0
Marek Sroczkowski <marekes97@gmail.com>
2021-02-22 22:45:18 +0100

    Added DbSets for entities

--- TutoringSystem/TutoringSystemAPI/AppDbContext.cs
+++ TutoringSystem/TutoringSystemAPI/AppDbContext.cs
@@ -3,12 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TutoringSystemLib.Entities;
 
 namespace TutoringSystemAPI
 {
     public class AppDbContext : DbContext
     {
         private readonly string connectionString = "Server=(localdb)\\mssqllocaldb;Database=TutoringSystemDb;Trusted_Connection=True;";
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Reservation> Reservations { get; set; }
+        public virtual DbSet<Lesson> Lessons { get; set; }
+        public virtual DbSet<Subject> Subjects { get; set; }
+        public virtual DbSet<Contact> Contacts { get; set; }
+        public virtual DbSet<PhoneNumber> PhoneNumbers { get; set; }
+        public virtual DbSet<AdditionalOrder> AdditionalOrders { get; set; }
+        public virtual DbSet<School> Schools { get; set; }
+        public virtual DbSet<Place> Places { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- TutoringSystem/TutoringSystemAPI/Startup.cs
+++ TutoringSystem/TutoringSystemAPI/Startup.cs
@@ -26,6 +26,7 @@ namespace TutoringSystemAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<AppDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
