dad0b9fb67a4ef541145c290b8ae1b643807bb54
Marek Sroczkowski <marekes97@gmail.com>
2021-02-22 22:37:49 +0100

    Added DbContext class

---
+++ TutoringSystem/TutoringSystemAPI/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutoringSystemAPI
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly string connectionString = "Server=(localdb)\\mssqllocaldb;Database=TutoringSystemDb;Trusted_Connection=True;";
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
