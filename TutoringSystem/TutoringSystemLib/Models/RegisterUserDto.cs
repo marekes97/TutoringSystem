91ea9f7d62d6d60a1e963f903ba06cfd72b40970
Marek Sroczkowski <marekes97@gmail.com>
2021-02-23 23:22:41 +0100

    Added dto for user register:

---
+++ TutoringSystem/TutoringSystemLib/Models/RegisterUserDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TutoringSystemLib.Entities;
+
+namespace TutoringSystemLib.Models
+{
+    public class RegisterUserDto
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        public string Email { get; set; }
+
+        public string DiscordName { get; set; }
+
+        public string PhoneOwner { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string SchoolName { get; set; }
+        public EducationLevel EducationLevel { get; set; }
+        public int EducationYear { get; set; }
+    }
+}
