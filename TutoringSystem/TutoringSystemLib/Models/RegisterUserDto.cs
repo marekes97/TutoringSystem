1278a3c92d8b58b4adcb692c21659cc58e422473
Marek Sroczkowski <marekes97@gmail.com>
2021-02-24 17:32:38 +0100

    Properties Login changed to UserName in class RegisterUserDto

--- TutoringSystem/TutoringSystemLib/Models/RegisterUserDto.cs
+++ TutoringSystem/TutoringSystemLib/Models/RegisterUserDto.cs
@@ -7,7 +7,7 @@ namespace TutoringSystemLib.Models
 {
     public class RegisterUserDto
     {
-        public string Login { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
