f00b0bbdc3779ec955d8b588fec8d9e10adcd97d
Marek Sroczkowski <marekes97@gmail.com>
2021-02-24 16:44:59 +0100

    Added minor fixes

--- TutoringSystem/TutoringSystemLib/Models/RegisterUserDto.cs
+++ TutoringSystem/TutoringSystemLib/Models/RegisterUserDto.cs
@@ -8,21 +8,13 @@ namespace TutoringSystemLib.Models
     public class RegisterUserDto
     {
         public string Login { get; set; }
-
         public string Password { get; set; }
-
         public string FirstName { get; set; }
-
         public string LastName { get; set; }
-
         public string ConfirmPassword { get; set; }
-
         public string Email { get; set; }
-
         public string DiscordName { get; set; }
-
         public string PhoneOwner { get; set; }
-
         public string PhoneNumber { get; set; }
         public string SchoolName { get; set; }
         public EducationLevel EducationLevel { get; set; }
