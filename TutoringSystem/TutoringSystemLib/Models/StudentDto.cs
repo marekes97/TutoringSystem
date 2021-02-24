3ade5d06654907ec3fa4f3c879552382dbd7004e
Marek Sroczkowski <marekes97@gmail.com>
2021-02-24 17:30:27 +0100

    Added dto for student

---
+++ TutoringSystem/TutoringSystemLib/Models/StudentDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TutoringSystemLib.Models
+{
+    public class StudentDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
