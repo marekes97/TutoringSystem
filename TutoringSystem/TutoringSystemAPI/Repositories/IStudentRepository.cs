070f81889319a813001c5755585a79670c359283
Marek Sroczkowski <marekes97@gmail.com>
2021-02-26 18:23:55 +0100

    Added repository for students

---
+++ TutoringSystem/TutoringSystemAPI/Repositories/IStudentRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TutoringSystemLib.Entities;
+
+namespace TutoringSystemAPI.Repositories
+{
+    public interface IStudentRepository
+    {
+        void CreateStudent(Student student);
+        void DeleteStudent(Student student);
+        Student GetStudent(string userName);
+        ICollection<Student> GetStudents();
+        void UpdateStudent(Student student);
+    }
+}
---
+++ TutoringSystem/TutoringSystemAPI/Repositories/StudentRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TutoringSystemLib.Entities;
+
+namespace TutoringSystemAPI.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public StudentRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public ICollection<Student> GetStudents() => dbContext.Students.ToList();
+
+        public Student GetStudent(string userName)
+        {
+            return dbContext.Students
+                .FirstOrDefault(m => m.UserName.Replace(" ", "-").ToLower().Equals(userName.ToLower()));
+        }
+
+        public void CreateStudent(Student student)
+        {
+            dbContext.Students.Add(student);
+            dbContext.SaveChanges();
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            dbContext.Students.Update(student);
+            dbContext.SaveChanges();
+        }
+
+        public void DeleteStudent(Student student)
+        {
+            dbContext.Remove(student);
+            dbContext.SaveChanges();
+        }
+    }
+}
