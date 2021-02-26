36e8370c730b025b78f5be927069412c3389bc84
Marek Sroczkowski <marekes97@gmail.com>
2021-02-26 21:24:42 +0100

    Fixed method for update student

--- TutoringSystem/TutoringSystemAPI/Repositories/StudentRepository.cs
+++ TutoringSystem/TutoringSystemAPI/Repositories/StudentRepository.cs
@@ -32,14 +32,45 @@ namespace TutoringSystemAPI.Repositories
         public void UpdateStudent(string userName, Student newStudent)
         {
             var student = GetStudent(userName);
-            student.Address = newStudent.Address;
-            student.Contact = newStudent.Contact;
+            var address = dbContext.Addresses.FirstOrDefault(a => a.User.Equals(student));
+            var contact = dbContext.Contacts.FirstOrDefault(c => c.User.Equals(student));
+            var school = dbContext.Schools.FirstOrDefault(s => s.Student.Equals(student));
+            var phones = dbContext.PhoneNumbers.Where(p => p.Contact.Equals(contact)).ToList();
+
+            address.City = newStudent.Address.City;
+            address.PostalCode = newStudent.Address.PostalCode;
+            address.Street = newStudent.Address.Street;
+
+            contact.DiscordName = newStudent.Contact.DiscordName;
+            contact.Email = newStudent.Contact.Email;
+            for (int i = 0; i < phones.Count; i++)
+            {
+                phones[i].Number = newStudent.Contact.PhoneNumbers[i].Number;
+                phones[i].Owner = newStudent.Contact.PhoneNumbers[i].Owner;
+            }
+            if(newStudent.Contact.PhoneNumbers.Count > phones.Count)
+            {
+                for (int i = phones.Count; i < newStudent.Contact.PhoneNumbers.Count; i++)
+                {
+                    phones[i].Number = newStudent.Contact.PhoneNumbers[i].Number;
+                    phones[i].Owner = newStudent.Contact.PhoneNumbers[i].Owner;
+                }
+            }
+
+            school.EducationLevel = newStudent.School.EducationLevel;
+            school.Name = newStudent.School.Name;
+            school.Year = newStudent.School.Year;
+
             student.FirstName = newStudent.FirstName;
             student.LastName = newStudent.LastName;
             student.PhotoUrl = newStudent.PhotoUrl;
             student.PasswordHash = newStudent.PasswordHash;
-            student.School = newStudent.School;
             student.UserName = newStudent.UserName;
+
+            dbContext.Students.Update(student);
+            dbContext.Addresses.Update(address);
+            dbContext.Contacts.Update(contact);
+            dbContext.Schools.Update(school);
 
             dbContext.SaveChanges();
         }
