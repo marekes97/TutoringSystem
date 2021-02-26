38932ec3a73bc64f2183a6e44b614c2c53e655b9
Marek Sroczkowski <marekes97@gmail.com>
2021-02-26 21:22:28 +0100

    Created endpoint for /api/student/{userName}

--- TutoringSystem/TutoringSystemAPI/Controllers/AccountController.cs
+++ TutoringSystem/TutoringSystemAPI/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -6,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TutoringSystemAPI.Identity;
+using TutoringSystemAPI.Repositories;
 using TutoringSystemLib.Entities;
 using TutoringSystemLib.Models;
 
@@ -14,15 +16,19 @@ namespace TutoringSystemAPI.Controllers
     [Route("api/account")]
     public class AccountController : ControllerBase
     {
+        private readonly IStudentRepository students;
         private readonly AppDbContext dbContext;
         private readonly IPasswordHasher<User> passwordHasher;
         private readonly IJwtProvider jwtProvider;
+        private readonly IMapper mapper;
 
-        public AccountController(AppDbContext dbContext, IPasswordHasher<User> passwordHasher, IJwtProvider jwtProvider)
+        public AccountController(IStudentRepository students, AppDbContext dbContext, IPasswordHasher<User> passwordHasher, IJwtProvider jwtProvider, IMapper mapper)
         {
+            this.students = students;
             this.dbContext = dbContext;
             this.passwordHasher = passwordHasher;
             this.jwtProvider = jwtProvider;
+            this.mapper = mapper;
         }
 
         [HttpPost("register")]
@@ -31,52 +37,20 @@ namespace TutoringSystemAPI.Controllers
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            var newUser = new Student
-            {
-                Role = Role.Student,
-                FirstName = registerUserDto.FirstName,
-                LastName = registerUserDto.LastName,
-                Login = registerUserDto.Login,
-                Address = new Address
-                {
-                    Street = registerUserDto.Street,
-                    City = registerUserDto.City,
-                    PostalCode = registerUserDto.PostalCode
-                },
-                Contact = new Contact
-                {
-                    DiscordName = registerUserDto.DiscordName,
-                    Email = registerUserDto.Email,
-                    PhoneNumbers = new List<PhoneNumber>
-                    {
-                        new PhoneNumber
-                        {
-                            Owner = registerUserDto.PhoneOwner,
-                            Number = registerUserDto.PhoneNumber
-                        }
-                    }
-                },
-                School = new School
-                {
-                    Name = registerUserDto.SchoolName,
-                    EducationLevel = registerUserDto.EducationLevel,
-                    Year = registerUserDto.EducationYear
-                }
-            };
+            var newUser = mapper.Map<Student>(registerUserDto);
+            newUser.Role = Role.Student;
 
             var passwordHash = passwordHasher.HashPassword(newUser, registerUserDto.Password);
             newUser.PasswordHash = passwordHash;
 
-            dbContext.Users.Add(newUser);
-            dbContext.SaveChanges();
-
+            students.CreateStudent(newUser);
             return Ok();
         }
 
         [HttpPost("login")]
-        public ActionResult Register([FromBody]LoginUserDto loginUserDto)
+        public ActionResult Login([FromBody]LoginUserDto loginUserDto)
         {
-            var user = dbContext.Users.FirstOrDefault(u => u.Login.Equals(loginUserDto.UserName));
+            var user = dbContext.Users.FirstOrDefault(u => u.UserName.Equals(loginUserDto.UserName));
 
             if (user == null)
                 return BadRequest("Invalid username of password!");
---
+++ TutoringSystem/TutoringSystemAPI/Controllers/StudentController.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TutoringSystemAPI.Repositories;
+using TutoringSystemLib.Entities;
+using TutoringSystemLib.Models;
+
+namespace TutoringSystemAPI.Controllers
+{
+    [Route("api/student")]
+    [Authorize]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentRepository students;
+        private readonly IMapper mapper;
+        private readonly IPasswordHasher<User> passwordHasher;
+
+        public StudentController(IStudentRepository students, IMapper mapper, IPasswordHasher<User> passwordHasher)
+        {
+            this.students = students;
+            this.mapper = mapper;
+            this.passwordHasher = passwordHasher;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Tutor")]
+        public ActionResult<List<StudentDto>> Get()
+        {
+            var studentsDtos = mapper.Map<List<StudentDto>>(students.GetStudents());
+            return Ok(studentsDtos);
+        }
+
+        [HttpGet("{userName}")]
+        [Authorize(Roles = "Tutor")]
+        public ActionResult<StudentDto> Get(string userName)
+        {
+            var student = students.GetStudent(userName);
+            if (student == null)
+                return NotFound();
+
+            var studentDto = mapper.Map<StudentDetailsDto>(student);
+            return Ok(studentDto);
+        }
+
+        [HttpPut("{userName}")]
+        [Authorize(Roles = "Tutor")]
+        public ActionResult Put(string userName, [FromBody] RegisterUserDto userDto)
+        {
+            var student = students.GetStudent(userName);
+            if (student == null)
+                return NotFound();
+
+            var newStudent = mapper.Map<Student>(userDto);
+            newStudent.PasswordHash = passwordHasher.HashPassword(newStudent, userDto.Password);
+            students.UpdateStudent(userName, newStudent);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{userName}")]
+        [Authorize(Roles = "Tutor")]
+        public ActionResult Delete(string userName)
+        {
+            var student = students.GetStudent(userName);
+            if (student == null)
+                return NotFound();
+
+            students.DeleteStudent(student);
+            return NoContent();
+        }
+    }
+}
