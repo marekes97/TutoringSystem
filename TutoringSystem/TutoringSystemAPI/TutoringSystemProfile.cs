0969090d4ad75049298511b5954d78c2d6b47c34
Marek Sroczkowski <marekes97@gmail.com>
2021-02-26 21:20:57 +0100

    Added new map for mapper

--- TutoringSystem/TutoringSystemAPI/TutoringSystemProfile.cs
+++ TutoringSystem/TutoringSystemAPI/TutoringSystemProfile.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TutoringSystemLib.Entities;
+using TutoringSystemLib.Models;
 
 namespace TutoringSystemAPI
 {
@@ -10,7 +12,18 @@ namespace TutoringSystemAPI
     {
         public TutoringSystemProfile()
         {
-            
+            CreateMap<AddressDto, Address>();
+            CreateMap<SchoolDto, School>()
+                .ForMember(s => s.Name, map => map.MapFrom(sDto => sDto.SchoolName));
+            CreateMap<ContactDto, Contact>();
+            CreateMap<PhoneNumberDto, PhoneNumber>();
+
+            CreateMap<Student, StudentDto>();
+            CreateMap<Student, StudentDetailsDto>();
+            CreateMap<RegisterUserDto, Student>()
+                .ForMember(u => u.Address, map => map.MapFrom(uDto => uDto.Address))
+                .ForMember(u => u.School, map => map.MapFrom(uDto => uDto.School))
+                .ForMember(u => u.Contact, map => map.MapFrom(uDto => uDto.Contact));
         }
     }
 }
