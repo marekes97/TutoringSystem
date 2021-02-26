9fc7e190f1fc626b55e74eded7608dccdb8c85ee
Marek Sroczkowski <marekes97@gmail.com>
2021-02-26 21:59:21 +0100

    Added dto model for additional order

---
+++ TutoringSystem/TutoringSystemLib/Models/OrderDetailsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TutoringSystemLib.Models
+{
+    public class OrderDetailsDto
+    {
+        public string Name { get; set; }
+        public DateTime Deadline { get; set; }
+        public string Description { get; set; }
+        public double Cost { get; set; }
+    }
+}
---
+++ TutoringSystem/TutoringSystemLib/Models/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TutoringSystemLib.Models
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
