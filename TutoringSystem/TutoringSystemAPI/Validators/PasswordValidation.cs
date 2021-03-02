63238c9660333f871e715b0f707d17238c9045df
Marek Sroczkowski <marekes97@gmail.com>
2021-03-02 14:09:15 +0100

    Added fluent validation for change password

---
+++ TutoringSystem/TutoringSystemAPI/Validators/PasswordValidation.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TutoringSystemLib.Entities;
+using TutoringSystemLib.Models;
+
+namespace TutoringSystemAPI.Validators
+{
+    public class PasswordValidation : AbstractValidator<PasswordDto>
+    {
+        public PasswordValidation()
+        {
+            RuleFor(p => p.NewPassword).MinimumLength(4);
+            RuleFor(p => p.NewPassword).Equal(p => p.ConfirmPassword);
+        }
+    }
+}
