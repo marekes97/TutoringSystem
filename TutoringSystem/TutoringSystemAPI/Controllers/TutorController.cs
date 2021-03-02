c4d3ea9d4cca24dbd6c372fb7802b6b4ba0a7ddb
Marek Sroczkowski <marekes97@gmail.com>
2021-03-02 14:08:31 +0100

    Added empty controller for  endpoint /api/tutor

---
+++ TutoringSystem/TutoringSystemAPI/Controllers/TutorController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TutoringSystemAPI.Repositories;
+
+namespace TutoringSystemAPI.Controllers
+{
+    [Route("api/tutor")]
+    [Authorize]
+    public class TutorController : ControllerBase
+    {
+        private readonly ITutorRepository tutorRepo;
+        private readonly IMapper mapper;
+
+        public TutorController(ITutorRepository tutorRepo, IMapper mapper)
+        {
+            this.tutorRepo = tutorRepo;
+            this.mapper = mapper;
+        }
+
+        
+    }
+}
