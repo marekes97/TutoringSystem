1a02abf8ae41c6f3c5e4ba2d3de75b6645bba109
Marek Sroczkowski <marekes97@gmail.com>
2021-04-04 22:59:59 +0200

    Added repository for contact

---
+++ TutoringSystem/TutoringSystemAPI/Repositories/ContactRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using TutoringSystemLib.Entities;
+
+namespace TutoringSystemAPI.Repositories
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly AppDbContext dbContext;
+        private readonly IPhoneNumberRepository phoneNumberRepo;
+
+        public ContactRepository(AppDbContext dbContext, IPhoneNumberRepository phoneNumberRepo)
+        {
+            this.dbContext = dbContext;
+            this.phoneNumberRepo = phoneNumberRepo;
+        }
+
+        public ICollection<Contact> GetContacts() => dbContext.Contacts
+            .Include(c => c.User)
+            .Include(c => c.PhoneNumbers)
+            .ToList();
+
+        public Contact GetContact(int id) => GetContacts()
+            .FirstOrDefault(c => c.Id.Equals(id));
+
+        public Contact GetContact(User user) => GetContacts()
+            .FirstOrDefault(c => c.User.Equals(user));
+
+        public void UpdateContact(Contact oldContact, Contact newContact)
+        {
+            oldContact.DiscordName = newContact.DiscordName;
+            oldContact.Email = newContact.Email;
+
+            for (int i = 0; i < oldContact.PhoneNumbers.Count; i++)
+                phoneNumberRepo.UpdatePhoneNumber(oldContact.PhoneNumbers[i], newContact.PhoneNumbers[i]);
+
+            if(newContact.PhoneNumbers.Count > oldContact.PhoneNumbers.Count)
+                for (int i = oldContact.PhoneNumbers.Count; i < newContact.PhoneNumbers.Count; i++)
+                    oldContact.PhoneNumbers.Add(newContact.PhoneNumbers[i]);
+
+            dbContext.Contacts.Update(oldContact);
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ TutoringSystem/TutoringSystemAPI/Repositories/IContactRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using TutoringSystemLib.Entities;
+
+namespace TutoringSystemAPI.Repositories
+{
+    public interface IContactRepository
+    {
+        Contact GetContact(int id);
+        Contact GetContact(User user);
+        ICollection<Contact> GetContacts();
+        void UpdateContact(Contact oldContact, Contact newContact);
+    }
+}
