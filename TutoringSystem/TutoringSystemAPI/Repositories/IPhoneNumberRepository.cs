1da17e8189d01f14b74ff243e87ed5856de8538c
Marek Sroczkowski <marekes97@gmail.com>
2021-04-04 23:01:43 +0200

    Added repository for phone number

---
+++ TutoringSystem/TutoringSystemAPI/Repositories/IPhoneNumberRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TutoringSystemLib.Entities;
+
+namespace TutoringSystemAPI.Repositories
+{
+    public interface IPhoneNumberRepository
+    {
+        PhoneNumber GetPhoneNumber(int id);
+        ICollection<PhoneNumber> GetPhoneNumbers();
+        void UpdatePhoneNumber(PhoneNumber oldPhoneNumber, PhoneNumber newPhoneNumber);
+    }
+}
---
+++ TutoringSystem/TutoringSystemAPI/Repositories/PhoneNumberRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TutoringSystemLib.Entities;
+
+namespace TutoringSystemAPI.Repositories
+{
+    public class PhoneNumberRepository : IPhoneNumberRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public PhoneNumberRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public ICollection<PhoneNumber> GetPhoneNumbers() => dbContext.PhoneNumbers
+            .Include(p => p.Contact)
+            .ToList();
+
+        public PhoneNumber GetPhoneNumber(int id) => GetPhoneNumbers()
+            .FirstOrDefault(p => p.Id.Equals(id));
+
+        public void UpdatePhoneNumber(PhoneNumber oldPhoneNumber, PhoneNumber newPhoneNumber)
+        {
+            oldPhoneNumber.Number = newPhoneNumber.Number;
+            oldPhoneNumber.Owner = newPhoneNumber.Owner;
+
+            dbContext.PhoneNumbers.Update(oldPhoneNumber);
+            dbContext.SaveChanges();
+        }
+    }
+}
