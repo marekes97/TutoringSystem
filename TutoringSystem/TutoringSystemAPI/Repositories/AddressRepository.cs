4cf80dc2eb8a451dff715c958b60c42c6aa61167
Marek Sroczkowski <marekes97@gmail.com>
2021-04-04 22:59:16 +0200

    Added repository for addresss

---
+++ TutoringSystem/TutoringSystemAPI/Repositories/AddressRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using TutoringSystemLib.Entities;
+
+namespace TutoringSystemAPI.Repositories
+{
+    public class AddressRepository : IAddressRepository
+    {
+        public readonly AppDbContext dbContext;
+
+        public AddressRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public ICollection<Address> GetAddresses() => dbContext.Addresses
+            .Include(a => a.User)
+            .ToList();
+
+        public Address GetAddress(int id) => GetAddresses()
+            .FirstOrDefault(a => a.Id.Equals(id));
+
+        public Address GetAddress(Student student) => GetAddresses()
+            .FirstOrDefault(a => a.User.Equals(student));
+
+        public void UpdateAddress(Address oldAddress, Address newAddress)
+        {
+            oldAddress.City = newAddress.City;
+            oldAddress.PostalCode = newAddress.PostalCode;
+            oldAddress.Street = newAddress.Street;
+
+            dbContext.Addresses.Update(oldAddress);
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ TutoringSystem/TutoringSystemAPI/Repositories/IAddressRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using TutoringSystemLib.Entities;
+
+namespace TutoringSystemAPI.Repositories
+{
+    public interface IAddressRepository
+    {
+        Address GetAddress(int id);
+        Address GetAddress(Student student);
+        ICollection<Address> GetAddresses();
+        void UpdateAddress(Address oldAddress, Address newAddress);
+    }
+}
